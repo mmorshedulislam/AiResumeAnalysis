964115747d68859f0978fadbeed41b75bce1331f
MORSHED-HOME <morshed952640@gmail.com>
2023-06-26 15:58:24 +0600

    install langchain and setup langchain and pinecone

--- client-react/src/scripts/ingest-data.mjs
+++ client-react/src/scripts/ingest-data.mjs
@@ -1,15 +1,15 @@
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import { PineconeStore } from 'langchain/vectorstores/pinecone';
-import { pinecone } from '@/utils/pinecone-client';
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
-import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
-import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { pinecone } from "../utils/pinecone-client.js";
+// import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from "../config/pinecone.js";
+
 
 /* Name of directory to retrieve your files from 
    Make sure to add your PDF files inside the 'docs' folder
 */
-const filePath = 'docs/Resume of Md Morshedul Islam.pdf';
+const filePath = "../docs/Resume of Md Morshedul Islam.pdf";
 
 export const run = async () => {
   try {
@@ -18,7 +18,7 @@ export const run = async () => {
     //   '.pdf': (path) => new PDFLoader(path),
     // });
 
-    const loader = new PDFLoader(filePath)
+    const loader = new PDFLoader(filePath);
 
     // const loader = new PDFLoader(filePath);
     const rawDocs = await loader.load();
@@ -32,26 +32,26 @@ export const run = async () => {
     });
 
     const docs = await textSplitter.splitDocuments(rawDocs);
-    console.log('split docs', docs);
+    console.log("split docs", docs);
 
-    console.log('creating vector store...');
+    console.log("creating vector store...");
     /*create and store the embeddings in the vectorStore*/
     const embeddings = new OpenAIEmbeddings();
-    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+    const index = pinecone.Index(process.env.PINECONE_INDEX_NAME); //change to your own index name
 
     //embed the PDF documents
     await PineconeStore.fromDocuments(docs, embeddings, {
       pineconeIndex: index,
-      namespace: PINECONE_NAME_SPACE,
-      textKey: 'text',
+      namespace: process.env.PINECONE_NAME_SPACE,
+      textKey: "text",
     });
   } catch (error) {
-    console.log('error', error);
-    throw new Error('Failed to ingest your data');
+    console.log("error", error);
+    throw new Error("Failed to ingest your data");
   }
 };
 
 (async () => {
   await run();
-  console.log('ingestion complete');
+  console.log("ingestion complete");
 })();
