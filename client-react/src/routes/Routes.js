cd3e8ac594fef290c637841aca080b2201ae8687
jahidhasan234031 <jahidhasan234031@mail.com>
2023-06-26 21:30:42 +0600

    'done'

--- client-react/src/routes/Routes.js
+++ client-react/src/routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import ChatWithPDF from "../pages/ChatWithPDF/ChatWithPDF";
+import Login from "../pages/Form/Login/Login";
+import SignUp from "../pages/Form/SignUp/SignUp";
 const routes = createBrowserRouter([
   {
     path: "/",
@@ -12,11 +14,11 @@ const routes = createBrowserRouter([
       },
       {
         path: "/login",
-        element: <div>Login</div>,
+        element: <Login></Login>,
       },
       {
         path: "/signup",
-        element: <div>Signup</div>,
+        element: <SignUp></SignUp>,
       },
       {
         path: "/chat",
